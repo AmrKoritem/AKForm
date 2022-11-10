004c3a5cc74be6594a43badf6e832fa5347b1cfa
Amr Koritem <amr.koritem92@gmail.com>
2022-11-10 22:09:11 +0200

    Initial Commit

---
+++ AKForm/AKForm.h
@@ -0,0 +1,18 @@
+//
+//  AKForm.h
+//  AKForm
+//
+//  Created by Amr Koritem on 10/11/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AKForm.
+FOUNDATION_EXPORT double AKFormVersionNumber;
+
+//! Project version string for AKForm.
+FOUNDATION_EXPORT const unsigned char AKFormVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AKForm/PublicHeader.h>
+
+
