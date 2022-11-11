f355d9ffd4a3c739b880423351af444a0449e8f7
Amr Koritem <amr.koritem92@gmail.com>
2022-11-11 21:37:23 +0200

    starting point: text type field enabled for normal form field style

---
+++ Sources/AKForm/Helpers/FormDelegate.swift
@@ -0,0 +1,94 @@
+//
+//  FormDelegate.swift
+//  AKForm
+//
+//  Created by Amr Koritem on 10/11/2022.
+//
+
+import UIKit
+
+/// Data source of form screen.
+public protocol FormDataSource {
+    var fields: [Field] { get }
+    var dataMap: [Int: Any] { get set }
+    func header(for section: Int) -> UIView?
+    func footer(for section: Int) -> UIView?
+}
+
+/// Delegate of form screen.
+public class FormDelegate: NSObject {
+    private(set) var dataSource: FormDataSource?
+
+    init(dataSource: FormDataSource?) {
+        self.dataSource = dataSource
+    }
+}
+
+public extension UITableView {
+    func register(fields: [Field]) {
+        fields.forEach { field in
+            switch field.type {
+            case .text:
+                registerNib(TextFieldTableViewCell.self)
+            default:
+                break
+            }
+        }
+    }
+}
+
+extension FormDelegate: UITableViewDataSource, UITableViewDelegate {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        dataSource?.fields.count ?? 0
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let field = dataSource?.fields[safe: indexPath.row] else { return UITableViewCell() }
+        switch field.type {
+        case .text:
+            let data = dataSource?.dataMap[field.id] as? String
+            let cell = tableView.dequeueReusableCell(withIdentifier: TextFieldTableViewCell.reuseIdentifier)
+            guard let cell = cell as? TextFieldTableViewCell else { return UITableViewCell() }
+            cell.configure(
+                field: field,
+                textFieldText: data ?? "",
+                textFieldEditingHandler: field.textFieldEditingHandler ?? { [weak self] text in
+                    guard let text = text else { return }
+                    self?.dataSource?.dataMap[field.id] = text
+                },
+                textFieldEditingDidEndHandler: field.textFieldEditingDidEndHandler ?? { _ in
+                    tableView.reloadRows(at: [indexPath], with: .automatic)
+                }
+            )
+            switch data?.getValidationStatus(for: field.contentType.validationRegex) {
+            case .invalid:
+                cell.showError(message: field.invalidErrorMessage ?? "Please enter a valid entry")
+            case .missing:
+                cell.showError(message: field.emptyErrorMessage ?? "Please enter your data")
+            default:
+                let placeholder = field.textFieldStyle.placeholderStyle.text
+                let attributedPlaceholder = field.textFieldStyle.placeholderStyle.attributedText
+                cell.clearField(placeholder ?? attributedPlaceholder?.string ?? "")
+            }
+            return cell
+        default:
+            return UITableViewCell()
+        }
+    }
+
+    public func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        dataSource?.header(for: section)
+    }
+
+    public func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        dataSource?.header(for: section)?.frame.height ?? 0
+    }
+
+    public func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        dataSource?.footer(for: section)
+    }
+
+    public func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        dataSource?.footer(for: section)?.frame.height ?? 0
+    }
+}
---
+++ Sources/AKForm/Views/FormViewController.swift
@@ -0,0 +1,129 @@
+//
+//  FormViewController.swift
+//  AKForm
+//
+//  Created by Amr Koritem on 10/11/2022.
+//
+
+import UIKit
+
+open class FormViewController: UIViewController, FormDataSource {
+    open var cancelsTouchesInView: Bool {
+        false
+    }
+
+    open var isFormTopConstrintToSafeArea: Bool {
+        true
+    }
+
+    open var isFormBottomConstrintToSafeArea: Bool {
+        true
+    }
+
+    public var form: UITableView?
+
+    public lazy var formDelegate = FormDelegate(dataSource: self)
+
+    deinit {
+        removeKeyboardObservers()
+    }
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        setFormUI()
+        configureForm()
+        addKeyboardObservers()
+        hideKeyboardWhenTappedAround()
+    }
+
+    public func setFormUI() {
+        form = UITableView()
+        guard let form = form else { return }
+        view.addSubview(form)
+        form.translatesAutoresizingMaskIntoConstraints = false
+        let topAnchor = isFormTopConstrintToSafeArea ? view.safeAreaLayoutGuide.topAnchor : view.topAnchor
+        let bottomAnchor = isFormBottomConstrintToSafeArea ? view.safeAreaLayoutGuide.bottomAnchor : view.bottomAnchor
+        NSLayoutConstraint.activate([
+            form.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            form.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            form.topAnchor.constraint(equalTo: topAnchor, constant: 0),
+            form.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0)
+        ])
+    }
+
+    public func configureForm() {
+        form?.dataSource = formDelegate
+        form?.delegate = formDelegate
+        form?.register(fields: fields)
+    }
+
+    public func addKeyboardObservers() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardDidShow),
+            name: UIResponder.keyboardDidShowNotification,
+            object: nil)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardDidHide),
+            name: UIResponder.keyboardDidHideNotification,
+            object: nil)
+    }
+
+    public func removeKeyboardObservers() {
+        NotificationCenter.default.removeObserver(
+            self,
+            name: UIResponder.keyboardDidShowNotification,
+            object: nil)
+        NotificationCenter.default.removeObserver(
+            self,
+            name: UIResponder.keyboardDidHideNotification,
+            object: nil)
+    }
+
+    open func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tap.cancelsTouchesInView = cancelsTouchesInView
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc
+    public func keyboardDidShow(_ notification: Notification) {
+        let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue
+        guard let keyboardSize = keyboardFrame?.cgRectValue else { return }
+        form?.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+        form?.isScrollEnabled = true
+    }
+
+    @objc
+    public func keyboardDidHide(_ notification: Notification) {
+        form?.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+
+    @objc
+    public func dismissKeyboard() {
+        view.endEditing(true)
+    }
+
+    // MARK: - FormDataSource
+    open var fields: [Field] {
+        []
+    }
+
+    open var dataMap: [Int: Any] {
+        get {
+            [:]
+        }
+        set {
+            _ = newValue
+        }
+    }
+
+    open func header(for section: Int) -> UIView? {
+        nil
+    }
+
+    open func footer(for section: Int) -> UIView? {
+        nil
+    }
+}
