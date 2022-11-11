d403026e3907509ce80fd433574e5b9cc25885fb
Amr Koritem <amr.koritem92@gmail.com>
2022-11-11 21:54:54 +0200

    add swift package manager support

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AKForm",
+    platforms: [
+        .iOS(.v13),
+        .tvOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "AKForm",
+            targets: ["AKForm"]),
+    ],
+    targets: [
+        .target(
+            name: "AKForm",
+            dependencies: []),
+        .testTarget(
+            name: "AKFormTests",
+            dependencies: ["AKForm"]),
+    ],
+    swiftLanguageVersions: [
+        .v5
+    ]
+)
