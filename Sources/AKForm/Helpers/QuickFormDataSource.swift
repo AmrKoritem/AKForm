b3158b0bf0713793dd367e360c6bb325fa1d35be
Amr Koritem <amr.koritem92@gmail.com>
2023-06-23 01:46:38 +0300

    add data accessors in quick form data source

--- Sources/AKForm/Helpers/QuickFormDataSource.swift
+++ Sources/AKForm/Helpers/QuickFormDataSource.swift
@@ -12,16 +12,24 @@ public struct QuickFormDataSource {
     /// Form fields.
     let fields: [Field]
     /// Form data filled by the user.
-    public var dataMap: [Int: Any]
+    var dataMap: [Int: Any]
     /// Form header view.
     let header: UIView?
     /// Form footer view.
     let footer: UIView?
 
-    public init(fields: [Field], dataMap: [Int: Any], header: UIView?, footer: UIView?) {
+    public init(fields: [Field], initialDataMap: [Int: Any] = [:], header: UIView? = nil, footer: UIView? = nil) {
         self.fields = fields
-        self.dataMap = dataMap
+        self.dataMap = initialDataMap
         self.header = header
         self.footer = footer
+    }
+
+    public func getDataAt(id: Int) -> Any? {
+        dataMap[id]
+    }
+
+    public mutating func setData(_ data: Any, at id: Int) {
+        dataMap[id] = data
     }
 }
